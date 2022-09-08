9386d01bc1036125156e29faa593c250867d2084
Michaelfr22 <102773052+Michaelfr22@users.noreply.github.com>
2022-09-08 17:17:48 -0400

    Added event to listen for filter changes

--- static/js/app.js
+++ static/js/app.js
@@ -74,7 +74,8 @@ function updateFilters() {
   }
   
   // 2. Attach an event to listen for changes to each filter
-  
+    d3.selectAll(".filter").on("change", updateFilters);
+
   
   // Build the table when the page loads
   function buildTable(data) {
