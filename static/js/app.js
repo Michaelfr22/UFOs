1d8d185984f04a7196a8731fd019952d720ffea6
Michaelfr22 <102773052+Michaelfr22@users.noreply.github.com>
2022-09-08 17:12:57 -0400

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -24,22 +24,34 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
-
+var filters = {};
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
+    let changedElement = d3.select(this).select("input");
 
     // 4b. Save the value that was changed as a variable.
+    let changedValue = changedElement.property("value");
+    console.log(changedValue);
 
     // 4c. Save the id of the filter that was changed as a variable.
-
+    let filterID = changedElement.attr("id");
+    console.log(filterID);
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
- 
-  
+    if (changedValue) {
+      filters[filterID] = changedValue.toLowerCase();
+    }
+    else {
+      delete filters[filterID];
+    }
+    
+    // Show filters
+    console.log(filters)
+
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
@@ -49,18 +61,40 @@ function updateFilters() {
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    
+    var filteredData = tableData;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
+    Object.values(filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+    });
   
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filteredData);
   }
   
   // 2. Attach an event to listen for changes to each filter
   
   
   // Build the table when the page loads
-  buildTable(tableData);
+  function buildTable(data) {
+
+    // First, clear out any existing data
+    tbody.html("");
+
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+
+      // Append a row to the table body
+      let row = tbody.append("tr");
+
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
